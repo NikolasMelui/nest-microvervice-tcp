f139c3543a9dfa7e290105f726b60f7d3595d71a
nsladkii <nsladkii@voximplant.com>
2021-07-04 12:37:02 +0300

    Update arch to the microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Logger } from '@nestjs/common';
 import { MathService } from './math.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -7,7 +8,7 @@ export class AppController {
 
   private readonly logger = new Logger('AppController');
 
-  @Post('accumulate')
+  @MessagePattern('accumulate')
   accumulate(@Body('data') data: number[]): number {
     this.logger.log(`Accumulating ${data.toString()}`);
     return this.mathService.accumulate(data);
